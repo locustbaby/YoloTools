79dd34951c71e6fa6d10a28bdcd172230b95f51c
Sheldon <1415182877@qq.com>
2023-10-22 17:49:35 +0800

    yaml template v1

--- go/yaml-template/main.go
+++ go/yaml-template/main.go
@@ -1,23 +1,43 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
+	"strings"
 	"text/template"
 
 	"gopkg.in/yaml.v2"
 )
 
 func main() {
-	// 读取 values.yaml 文件
-	values, err := os.ReadFile("values.yaml")
+	// Define command-line flags
+	valuesFile := flag.String("values", "", "values file")
+	templateFile := flag.String("template", "", "template file or directory path")
+	outputDir := flag.String("output", "", "output directory path")
+
+	// Parse command-line flags
+	flag.Parse()
+
+	// Check if required flags are provided
+	if *templateFile == "" {
+		flag.PrintDefaults()
+		return
+	}
+
+	// Print flag values
+	fmt.Println("Template file path:", *templateFile)
+	fmt.Println("Output directory path:", *outputDir)
+
+	// Read values.yaml file
+	values, err := os.ReadFile(*valuesFile)
 	if err != nil {
 		fmt.Println("Error reading values.yaml:", err)
 		return
 	}
 
-	// 解析 values.yaml 文件，将其内容解析为 Go map
+	// Parse values.yaml file and convert its content to a Go map
 	valuesMap := make(map[string]interface{})
 	err = yaml.Unmarshal(values, &valuesMap)
 	if err != nil {
@@ -25,33 +45,61 @@ func main() {
 		return
 	}
 
-	// 遍历 templates 目录下的所有文件
-	templatesDir := "templates"
+	// Create the output directory
+	if *outputDir != "" {
+		err = os.MkdirAll(*outputDir, 0755)
+		if err != nil {
+			fmt.Println("Error creating output directory:", err)
+			return
+		}
+	}
+
+	// Traverse all files in the templates directory
+	templatesDir := *templateFile
 	err = filepath.Walk(templatesDir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
-		// 忽略目录
+		// Ignore directories
 		if info.IsDir() {
 			return nil
 		}
 
-		// 读取模板文件
+		// Read the template file
 		templateContent, err := os.ReadFile(path)
 		if err != nil {
 			return err
 		}
 
-		// 创建模板对象并解析模板
+		// Create and parse the template object
 		tmpl, err := template.New("template").Parse(string(templateContent))
 		if err != nil {
 			return err
 		}
 
-		// 渲染模板并输出到标准输出
-		err = tmpl.Execute(os.Stdout, valuesMap)
+		// Render the template
+		var outputContent strings.Builder
+		err = tmpl.Execute(&outputContent, valuesMap)
 		if err != nil {
 			return err
+		}
+
+		// Print to standard output
+		fmt.Println(outputContent.String())
+
+		// Write to file
+		if *outputDir != "" {
+			outputFileName := filepath.Join(*outputDir, info.Name())
+			outputFile, err := os.Create(outputFileName)
+			if err != nil {
+				return err
+			}
+			defer outputFile.Close()
+
+			_, err = outputFile.WriteString(outputContent.String())
+			if err != nil {
+				return err
+			}
 		}
 
 		return nil
